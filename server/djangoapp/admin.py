4e014ab0d80621c4f2e722c0f8eaaa620d83652f
QunshanWu <wuqunshan_2019@qq.com>
2025-07-04 23:37:56 -0400

    update

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,29 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1  # Un formulaire vide supplémentaire par défaut
+
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'dealer_id')
+    list_filter = ('type', 'year', 'car_make')
+    search_fields = ('name', 'car_make__name')
+
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
 
 # Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
